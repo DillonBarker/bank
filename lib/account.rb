bbb5381021b757bafb85ad5d26728ce97c333aae
DillonBarker <barker.dill98@gmail.com>
2020-09-07 15:24:19 +0100

    added the account memory mock test to account spec. The account memory can receive transactions which is an empty hash so that data can be stored in that class

--- lib/account.rb
+++ lib/account.rb
@@ -10,21 +10,24 @@ class Account
     def initialize(account_memory = AccountMemory.new)
         @account_memory = account_memory
         @balance = STARTING_BALANCE
-        @transactions = {}
     end
 
     def deposit(amount)
         @balance += amount
-        save_transaction
+        save_credit_transaction
     end
     
     def withdraw(amount)
         @balance -= amount
-        save_transaction
+        save_debit_transaction
     end
 
-    def save_transaction
-        
+    def save_credit_transaction
+        @account_memory.transactions[:date] = Time.now.strftime("%d/%m/%Y")
+    end
+
+    def save_debit_transaction
+        @account_memory.transactions[:date] = Time.now.strftime("%d/%m/%Y")
     end
 
     def get_balance
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,25 +3,34 @@ require 'account'
 describe Account do
     let(:account_memory_double) { instance_double(AccountMemory)}
     let(:account) { Account.new(account_memory_double) }
-   
+    let(:time_now) { Time.now.strftime("%d/%m/%Y") }
 
     it 'can return the balance of that account ( 0 ), using a method' do
         expect(account.get_balance).to eq Account::STARTING_BALANCE
     end
 
     it 'can deposit money into account, this will change the balance' do
+        allow(account).to receive(:save_credit_transaction)
         expect{ account.deposit(Account::EXAMPLE_AMOUNT) }.to change{ account.balance }.by Account::EXAMPLE_AMOUNT
         expect(account.get_balance).to eq Account::EXAMPLE_AMOUNT
     end
 
     it 'can withdraw money from an account, this will change the balance' do
+       allow(account).to receive(:save_credit_transaction)
        account.deposit(Account::EXAMPLE_AMOUNT)
        expect(account.get_balance).to eq(Account::EXAMPLE_AMOUNT)
+       allow(account).to receive(:save_debit_transaction)
        expect{ account.withdraw(Account::EXAMPLE_AMOUNT) }.to change{ account.get_balance }.by -Account::EXAMPLE_AMOUNT
        expect(account.get_balance).to eq Account::STARTING_BALANCE
     end
 
     it 'holds an instance of account memory' do
         expect(account.account_memory).to eq (account_memory_double)
+    end
+
+    it 'can store a the date when a transaction is made.' do
+        allow(account_memory_double).to receive(:transactions).and_return({})
+        account.deposit(Account::EXAMPLE_AMOUNT)
+        expect(account.account_memory.transactions).to eq(date: time_now)
     end
 end
