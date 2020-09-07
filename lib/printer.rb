3b93e3654e814aee98d7e72c7890b33c36c1b2d1
DillonBarker <barker.dill98@gmail.com>
2020-09-07 14:21:43 +0100

    Removed date, and time from the printer. It shouldnt belong here. The date is saved when making a deposit or withdrawal -> extract date to a class and use it in the account class

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,13 +1,12 @@
 class Printer
-    
+
     def print_statement_header
         return "date || credit || debit || balance"
     end
 
     def print_statement
-        time = Time.now.strftime("%d/%m/%Y")
         return print_statement_header + " \n " +
-        time
+        " || " + " || " + " || "
     end
     
 end
