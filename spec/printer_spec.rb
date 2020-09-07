675acd6a5a1debb065c14a30e1e178e7010056c4
DillonBarker <barker.dill98@gmail.com>
2020-09-07 11:05:52 +0100

    written test for printer being able to print empty statment

---
+++ spec/printer_spec.rb
@@ -0,0 +1,8 @@
+require 'printer'
+
+describe Printer do 
+    it 'prints out an empty statement, with columns date, credit, debit and balance' do
+        printer = Printer.new()
+        expect(printer.print_statement).to eq("date || credit || debit || balance")
+    end
+end
