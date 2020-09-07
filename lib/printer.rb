7a141a091a12410413637ed5cd2ac5a947630dd3
DillonBarker <barker.dill98@gmail.com>
2020-09-07 11:51:31 +0100

    added print statement method, which can add a fake date onto the printed result. Now need to incorporate Time.now

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,5 +3,10 @@ class Printer
     def print_statement_header
         return "date || credit || debit || balance"
     end
+
+    def print_statement
+        return print_statement_header + " \n " +
+        "01/01/2020"
+    end
     
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,4 +5,8 @@ describe Printer do
     it 'prints out an empty statement, with columns date, credit, debit and balance' do
         expect(printer.print_statement_header).to eq("date || credit || debit || balance")
     end
+
+    it 'prints out a transaction with a date following a statement header' do
+        expect(printer.print_statement).to eq ("date || credit || debit || balance \n 01/01/2020")
+    end
 end
