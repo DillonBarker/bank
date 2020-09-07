76fcbcd613e587e56369f20f3bfe7930598f32e9
DillonBarker <barker.dill98@gmail.com>
2020-09-07 17:34:22 +0100

    added the else if statement that builds the report differently based on whether or not it is a deposit or withdrawal

--- lib/printer.rb
+++ lib/printer.rb
@@ -7,7 +7,11 @@ class Printer
     def print_statement(transactions)
         print_statement_header
         statement = transactions.map do |transaction|
-            "#{transaction[:date]}  ||  #{transaction[:amount]}  ||  ||  #{transaction[:balance]}"
+            if transaction[:type] = 'credit'
+                "#{transaction[:date]}  ||  #{transaction[:amount]}  ||  ||  #{transaction[:balance]}"
+            else 
+                "#{transaction[:date]}  ||  || #{transaction[:amount]} ||  #{transaction[:balance]}"
+            end
         end
         print_statement_header + "\n" + statement.join
     end
