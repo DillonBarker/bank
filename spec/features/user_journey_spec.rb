d32e5694de1988eda29d962e8a5d1bb18be839c2
DillonBarker <barker.dill98@gmail.com>
2020-09-09 10:40:42 +0100

    stubbing date in the feature test

--- spec/features/user_journey_spec.rb
+++ spec/features/user_journey_spec.rb
@@ -6,12 +6,19 @@ describe 'User Journey' do
     let(:printer) { Printer.new() }
     let(:account_memory) { AccountMemory.new(printer) }
     let(:account) { Account.new(account_memory) }
-    let(:time_now) { Time.now.strftime('%d/%m/%Y') }
-    
+
+    def stub_date(chosen_date)
+        now = Time.parse(chosen_date)
+        allow(Time).to receive(:now) { now }
+    end
+
     it 'meets the acceptance criteria of the challenge' do
+        stub_date("10/01/2012")
         account.deposit(1000)
+        stub_date("13/01/2012")
         account.deposit(2000)
+        stub_date("14/01/2012")
         account.withdraw(500)
-        expect(account.print).to eq ("date || credit || debit || balance\n#{time_now}  ||  || 500.00 ||  2500.00\n#{time_now}  ||  2000.00  ||  ||  3000.00\n#{time_now}  ||  1000.00  ||  ||  1000.00")
+        expect(account.print).to eq ("date || credit || debit || balance\n14/01/2012  ||  || 500.00 ||  2500.00\n13/01/2012  ||  2000.00  ||  ||  3000.00\n10/01/2012  ||  1000.00  ||  ||  1000.00")
     end
 end
