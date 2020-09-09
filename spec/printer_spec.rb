f3d946bc6e3ad6f626c40b051f2ac12dc6f09468
DillonBarker <barker.dill98@gmail.com>
2020-09-09 10:42:49 +0100

    removed the time.now from printer as it was not needed, replaced it with an example date

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,26 +2,25 @@ require 'printer'
 
 describe Printer do
     let(:printer) { Printer.new }
-    let(:time_now) { Time.now.strftime('%d/%m/%Y') }
 
     it 'prints out an empty statement, with columns date, credit, debit and balance' do
         expect(printer.print_statement_header).to eq('date || credit || debit || balance')
     end
 
     it 'prints a statement' do
-        expect(printer.print_statement([{date: time_now, type: true, amount: 100, balance: 100 }])).to eq("date || credit || debit || balance\n#{time_now}  ||  100.00  ||  ||  100.00")
+        expect(printer.print_statement([{date: "10/10/2010", type: true, amount: 100, balance: 100 }])).to eq("date || credit || debit || balance\n10/10/2010  ||  100.00  ||  ||  100.00")
     end
 
     it 'can print a statement with 2 transactions' do
-        expect(printer.print_statement([{ date: time_now, type: true, amount: 100, balance: 100 },
-                                        { date: time_now, type: true, amount: 100, balance: 200 }])).to eq("date || credit || debit || balance\n#{time_now}  ||  100.00  ||  ||  200.00\n#{time_now}  ||  100.00  ||  ||  100.00")
-        expect(printer.print_statement([{ date: time_now, type: true, amount: 100, balance: 100 },
-                                        { date: time_now, type: false, amount: 100, balance: 0  }])).to eq("date || credit || debit || balance\n#{time_now}  ||  || 100.00 ||  0.00\n#{time_now}  ||  100.00  ||  ||  100.00")
+        expect(printer.print_statement([{ date: "10/10/2010", type: true, amount: 100, balance: 100 },
+                                        { date: "10/10/2010", type: true, amount: 100, balance: 200 }])).to eq("date || credit || debit || balance\n10/10/2010  ||  100.00  ||  ||  200.00\n10/10/2010  ||  100.00  ||  ||  100.00")
+        expect(printer.print_statement([{ date: "10/10/2010", type: true, amount: 100, balance: 100 },
+                                        { date: "10/10/2010", type: false, amount: 100, balance: 0  }])).to eq("date || credit || debit || balance\n10/10/2010  ||  || 100.00 ||  0.00\n10/10/2010  ||  100.00  ||  ||  100.00")
     end
 
     it 'can print a statement with multiple transactions' do
-        expect(printer.print_statement([{ date: time_now, type: true, amount: 100, balance: 100 },
-                                        { date: time_now, type: true, amount: 100, balance: 200 },
-                                        { date: time_now, type: true, amount: 100, balance: 300 }])).to eq("date || credit || debit || balance\n#{time_now}  ||  100.00  ||  ||  300.00\n#{time_now}  ||  100.00  ||  ||  200.00\n#{time_now}  ||  100.00  ||  ||  100.00")
+        expect(printer.print_statement([{ date: "10/10/2010", type: true, amount: 100, balance: 100 },
+                                        { date: "10/10/2010", type: true, amount: 100, balance: 200 },
+                                        { date: "10/10/2010", type: true, amount: 100, balance: 300 }])).to eq("date || credit || debit || balance\n10/10/2010  ||  100.00  ||  ||  300.00\n10/10/2010  ||  100.00  ||  ||  200.00\n10/10/2010  ||  100.00  ||  ||  100.00")
     end
 end
