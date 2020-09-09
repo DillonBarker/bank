603225e6d233e30d2260f6bdd808acfb5bc9c0f1
DillonBarker <barker.dill98@gmail.com>
2020-09-09 10:56:34 +0100

    adding a tranasaction class, testing its attriubtes

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,14 @@
+require 'transaction'
+
+describe Transaction do
+    
+    let(:transaction) { Transaction.new("10/10/2010", true, 100) }
+
+    it 'has attributes for the date, type of transaction, amount' do
+        expect(transaction.date).to eq("10/10/2010")
+        expect(transaction.type).to eq(true)
+        expect(transaction.amount).to eq(100)
+    end 
+
+    
+end
